929dd6f77394c56ddb8a11fc5a98829936ee7cd2
Jordan W. Taylor <football80@gmail.com>
2012-11-17 20:44:00 -0800

    trying something

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121116044456) do
+ActiveRecord::Schema.define(:version => 20121109210853) do
 
   create_table "comments", :force => true do |t|
     t.text     "comment"
@@ -21,14 +21,6 @@ ActiveRecord::Schema.define(:version => 20121116044456) do
     t.datetime "updated_at", :null => false
     t.string   "activity"
     t.string   "email"
-  end
-
-  create_table "users", :force => true do |t|
-    t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at",                            :null => false
-    t.datetime "updated_at",                            :null => false
-    t.string   "role",            :default => "author"
   end
 
 end
