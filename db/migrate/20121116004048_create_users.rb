f409fb186613818828e652230a1bf0a206df4e53
Jordan W. Taylor <football80@gmail.com>
2012-11-17 20:50:32 -0800

    this better work

--- db/migrate/20121116004048_create_users.rb
+++ db/migrate/20121116004048_create_users.rb
@@ -1,10 +1,21 @@
 class CreateUsers < ActiveRecord::Migration
   def change
-    create_table :users do |t|
+
+    create_table :users, :force => true do |t|
       t.string :email
       t.string :password_digest
+      t.string :role, :default => "author"
 
       t.timestamps
     end
   end
 end
+
+
+
+# ActiveRecord::Schema.define(:version => 20080906171750) do
+#   create_table "authors", :force => true do |t|
+#     t.string   "name"
+#     t.datetime "created_at"
+#     t.datetime "updated_at"
+#   end
