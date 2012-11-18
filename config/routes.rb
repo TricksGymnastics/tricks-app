b0ed0f23cd7195a137409ae71aae827dbed37407
Jordan W. Taylor <football80@gmail.com>
2012-11-17 19:51:20 -0800

    Trying to fix previous commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Comments::Application.routes.draw do
+
+  root :to => 'home#index'
+
+  resources :comments
+  resources :users
+  resources :sessions 
+
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
-
-  resources :users
-  resources :sessions
-
-  root :to => 'home#index'
-  
-  resources :comments
 
 end
