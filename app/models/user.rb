34a57363a9e1804e8a67f55566d74cf3723bdb66
Jordan W. Taylor <football80@gmail.com>
2012-12-19 13:35:03 -0800

    fixed the ablitiy to assing a role to user, and change truncate length of quotes on index page.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 	has_secure_password
-	attr_accessible :email, :password, :password_confirmation
+	attr_accessible :email, :password, :password_confirmation, :role
 	validates_uniqueness_of :email
 
 	def self.search(search)
