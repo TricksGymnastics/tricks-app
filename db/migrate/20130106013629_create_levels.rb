3814891a71325a9b7531ed52e8c778655f8a6d56
Jordan W. Taylor <football80@gmail.com>
2013-01-26 12:49:43 -0800

    fixing migrations

--- db/migrate/20130106013629_create_levels.rb
+++ db/migrate/20130106013629_create_levels.rb
@@ -1,12 +1,12 @@
 class CreateLevels < ActiveRecord::Migration
   def change
-    create_table :levels do |t|
-      t.string :levelname
-      t.integer :length
-      t.integer :price
-      t.integer :age
+    #create_table :levels do |t|
+      #t.string :levelname
+     # t.integer :length
+     # t.integer :price
+     # t.integer :age
 
-      t.timestamps
+     # t.timestamps
     end
   end
 end
