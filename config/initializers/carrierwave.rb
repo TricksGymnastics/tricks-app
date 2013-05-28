1106a8b47be67f005672ffd881b5f4d81e6a90c8
Jordan W. Taylor <football80@gmail.com>
2013-05-27 22:35:24 -0700

    Added Surveys, fixed schedules

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,10 @@
 CarrierWave.configure do |config|
-  # config.fog_credentials = {
-  #   :provider                         => 'Google',
-  #   :google_storage_access_key_id     => ENV['GOOGLE_KEY_ID'],  
-  #   :google_storage_secret_access_key => ENV['GOOGLE_ACCESS_KEY']
-  # }
-
   config.fog_credentials = {
     :provider                         => 'Google',
-    :google_storage_access_key_id     => 'GOOGHPIH4JRNVFHDHAXO',  
-    :google_storage_secret_access_key => '+m9YwS0YlqmQVMAvtsSFnvGxdouZllAA6F6VvWff'
+    :google_storage_access_key_id     => ENV['GOOGLE_KEY_ID'],  
+    :google_storage_secret_access_key => ENV['GOOGLE_ACCESS_KEY']
   }
+
 
   #config.fog_directory = @fog_directory
 end
