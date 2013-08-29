ebfc6d9f99b1c2bd045b678abb2e32b36dfe2856
Jordan W. Taylor <football80@gmail.com>
2013-08-28 18:50:21 -0700

    Added Danish Warmup to Tricksu

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,4 @@ CarrierWave.configure do |config|
     :google_storage_access_key_id     => ENV['GOOGLE_KEY_ID'],  
     :google_storage_secret_access_key => ENV['GOOGLE_ACCESS_KEY']
   }
-
-
-  #config.fog_directory = @fog_directory
 end
