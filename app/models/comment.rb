ac72de8e82368974507ab0cc2200ad9d34190951
Jordan W. Taylor <football80@gmail.com>
2014-01-08 16:18:04 -0700

    Fixed bug with comments validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,7 +4,7 @@ class Comment < ActiveRecord::Base
 
   #validates :comment, exclusion: { in: %w(www http href href="), message: "%{value} is reserved." }
   #validates_format_of :comment, :without => /\A(<a href=|http)\Z/
-  validates_format_of :comment, :with => /\A(?!www|!http|!<a href=)\Z/, message: "cannot contain a url."
+  validates_format_of :comment, :without => /(www|http|href)/, message: "cannot contain a url."
 
 	def self.search(search)
   		if search
