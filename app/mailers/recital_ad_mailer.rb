728569d2fca29a262acc4078e454761fc09b93d6
Jordan W. Taylor <football80@gmail.com>
2014-05-16 13:02:09 -0600

    added recital ad purchasing back to main menu and added a confirmation email to be sent after an order is made

--- app/mailers/recital_ad_mailer.rb
+++ app/mailers/recital_ad_mailer.rb
@@ -1,5 +1,13 @@
 class RecitalAdMailer < ActionMailer::Base
-  default from: "Tricks Gymnastics, Dance & Swim <tricksgym@gmail.com>"
+require 'mail'
+email = "tricksgym@gmail.com"
+address = Mail::Address.new email # ex: "john@example.com"
+name = "Tricks Gymnastics, Dance & Swim"
+address.display_name = name # ex: "John Doe"
+# Set the From or Reply-To header to the following:
+address.format # returns "John Doe <john@example.com>"
+
+  default from: address
 
   def order_confirmation(recital_ad)
     @recital_ad = recital_ad
