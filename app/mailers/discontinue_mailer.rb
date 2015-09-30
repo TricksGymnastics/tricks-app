a8f971aa582064a80ff42509d38f6d05f3d58e68
Jordan Taylor <football80@gmail.com>
2015-09-30 00:49:45 -0600

    Discontinue notice now sends out an email

--- app/mailers/discontinue_mailer.rb
+++ app/mailers/discontinue_mailer.rb
@@ -9,14 +9,17 @@ class DiscontinueMailer < ActionMailer::Base
   def gym_notification(notification)
     @notification = notification
 
-    if (DiscontinueNotice::LOCATIONS[@discontinue_notice.location][0] == "Granite Bay")
+    if (DiscontinueNotice::LOCATIONS[@notification.location][0] == "Granite Bay")
       mail to: "tricksgb@gmail.com", subject: "Notice of Discontinuation for: " + @notification.student_first_name + " " + @notification.student_last_name
-    elsif (DiscontinueNotice::LOCATIONS[@discontinue_notice.location][0] == "Folsom")
+    elsif (DiscontinueNotice::LOCATIONS[@notification.location][0] == "Folsom")
       mail to: "tricksfol@gmail.com", subject: "Notice of Discontinuation for: " + @notification.student_first_name + " " + @notification.student_last_name
-    elsif (DiscontinueNotice::LOCATIONS[@discontinue_notice.location][0] == "Sacramento")
+    elsif (DiscontinueNotice::LOCATIONS[@notification.location][0] == "Sacramento")
       mail to: "trickssac@gmail.com", subject: "Notice of Discontinuation for: " + @notification.student_first_name + " " + @notification.student_last_name
     end
 
     # mail to: "football80@gmail.com", subject: "This better work " + @notification.student_first_name
   end
 end
+
+
+http://www.tricksgym.com/discontinue_notices
