73dd856a504f82b1a91e5bb97b868a46872978a3
Jordan Taylor <football80@gmail.com>
2015-10-17 21:12:07 -0600

    Still trying to fix the email bug

--- app/mailers/discontinue_mailer.rb
+++ app/mailers/discontinue_mailer.rb
@@ -8,15 +8,18 @@ class DiscontinueMailer < ActionMailer::Base
 
   def gym_notification(notification)
     @notification = notification
-
+    @subject = "Notice of Discontinuation for: " + @notification.student_first_name + " " + @notification.student_last_name
+    gym_email = "football80@gmail.com"
     if (DiscontinueNotice::LOCATIONS[@notification.location][0] == "Granite Bay")
-      mail to: "tricksgb@gmail.com", subject: "Notice of Discontinuation for: " + @notification.student_first_name + " " + @notification.student_last_name
-    elsif (DiscontinueNotice::LOCATIONS[@notification.location][1] == "Folsom")
-      mail to: "tricksfol@gmail.com", subject: "Notice of Discontinuation for: " + @notification.student_first_name + " " + @notification.student_last_name
-    elsif (DiscontinueNotice::LOCATIONS[@notification.location][2] == "Sacramento")
-      mail to: "trickssac@gmail.com", subject: "Notice of Discontinuation for: " + @notification.student_first_name + " " + @notification.student_last_name
+      gym_email = "tricksgb@gmail.com"
+    elsif (DiscontinueNotice::LOCATIONS[@notification.location][0] == "Folsom")
+      gym_email = "tricksfol@gmail.com"
+    elsif (DiscontinueNotice::LOCATIONS[@notification.location][0] == "Sacramento")
+      gym_email = "trickssac@gmail.com"
+    else
+      @subject = "Something went wrong in discontinue_mailer and a gym didn't receive this submission"
     end
-
-    # mail to: "football80@gmail.com", subject: "This better work " + @notification.student_first_name
+    
+    mail to: gym_email, subject: @subject
   end
 end
