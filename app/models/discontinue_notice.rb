0e4929c2fc73499a53f0ba890ccb9d3785594498
Jordan Taylor <football80@gmail.com>
2015-10-23 18:46:47 -0600

    Updated Newsletter, added server side required flags for fields in discontinue notices and tricks_u_videos

--- app/models/discontinue_notice.rb
+++ app/models/discontinue_notice.rb
@@ -1,6 +1,7 @@
 class DiscontinueNotice < ActiveRecord::Base
 	attr_accessible :class_day, :class_time, :last_day, :location, :parent_name, :reason, :student_first_name, :student_last_name, :understood
 
+    validates_presence_of :class_day, :class_time, :last_day, :location, :parent_name, :reason, :student_last_name, :student_first_name, :understood
 
 	LOCATIONS = [['Granite Bay', 0], ['Folsom', 1], ['Sacramento', 2]]
 	DAYS = [['Monday', 0], ['Tuesday', 1], ['Wednesday', 2], ['Thursday', 3], ['Friday', 4], ['Saturday', 5]]
