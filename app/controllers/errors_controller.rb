fefa580962ef885644bc6db31a3da157906298bd
Jordan Taylor <football80@gmail.com>
2016-05-12 18:57:56 +0000

    No longer sending error emails for 404 not found

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,10 +1,12 @@
 class ErrorsController < ApplicationController
     layout 'static'
 	def show
-		@exception = env["action_dispatch.exception"]
-		render action: request.path[1..-1]
-		email = ErrorMailer.error_details(@exception)
-		email.delivery_method.settings.merge!({user_name: ENV['GMAIL_WEBMASTER_ID'], password: ENV['GMAIL_WEBMASTER_PASSWORD']})
-		email.deliver
-	end  
+		if params[:status] != "404"
+			@exception = env["action_dispatch.exception"]
+			render action: request.path[1..-1]
+			email = ErrorMailer.error_details(@exception)
+			email.delivery_method.settings.merge!({user_name: ENV['GMAIL_WEBMASTER_ID'], password: ENV['GMAIL_WEBMASTER_PASSWORD']})
+			email.deliver
+		end
+	end
 end
