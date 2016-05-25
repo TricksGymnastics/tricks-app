d8e9c28ea7e60e81a21e4d441aa187f2c9ad5226
Jordan Taylor <football80@gmail.com>
2016-05-25 18:19:17 +0000

    removed a few pages that no longer exist, prompting people to update their bookmarks

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,24 @@
 Comments::Application.routes.draw do
 
-  resources :bubble_contents
   root :to => 'static#index'
 
   get '(errors)/:status', to: 'errors#show', constraints: {status: /\d{3}/}  
 
-  get 'schedules/choose'  
-  get 'schedules/gb_gym'
-  get 'schedules/gb_tb'
-  get 'schedules/gb_dance'
-  get 'schedules/gb_tag'
-  get 'schedules/fol_gym'
-  get 'schedules/fol_tb'
-  get 'schedules/fol_dance'
-  get 'schedules/fol_swim'
-  get 'schedules/fol_tag'
-  get 'schedules/sac_gym'
-  get 'schedules/sac_tb'
-  get 'schedules/sac_dance'
-  get 'schedules/sac_tag'
-  get 'schedules/by_gym'
+  # get 'schedules/choose'  
+  # get 'schedules/gb_gym'
+  # get 'schedules/gb_tb'
+  # get 'schedules/gb_dance'
+  # get 'schedules/gb_tag'
+  # get 'schedules/fol_gym'
+  # get 'schedules/fol_tb'
+  # get 'schedules/fol_dance'
+  # get 'schedules/fol_swim'
+  # get 'schedules/fol_tag'
+  # get 'schedules/sac_gym'
+  # get 'schedules/sac_tb'
+  # get 'schedules/sac_dance'
+  # get 'schedules/sac_tag'
+  # get 'schedules/by_gym'
 
   get 'coaches/type/:name' => "coaches#type"
   get 'coaches/loc/:name' => "coaches#loc"
@@ -31,9 +30,9 @@ Comments::Application.routes.draw do
 
   get 'datenights/edit' => 'datenights#edit'
 
-  get 'choose_survey' => 'survey_results#choose_survey' 
-  get 'survey_result_for/:name' => 'survey_results#survey_results_for' 
-  get 'survey_results/:id' => 'survey_results#show' 
+  # get 'choose_survey' => 'survey_results#choose_survey' 
+  # get 'survey_result_for/:name' => 'survey_results#survey_results_for' 
+  # get 'survey_results/:id' => 'survey_results#show' 
 
   resources :comments
   resources :kid_quotes
@@ -54,31 +53,29 @@ Comments::Application.routes.draw do
   resources :tricks_u_videos
   resources :website_pdfs
   resources :discontinue_notices
+  resources :bubble_contents
   # resources :training_videos
 
-  %w[gymnastics tumblebunnies tag dance preschool_dance swim events locations site_comments hosting competitive teamgym 
-    birthdays princess_party camps dancecamps campus employment forms gymnastics25 
-    missing nutcracker polkadots recitals recital_ad_order_thank_you show_schedules survey_thank_you 
-    summer thankyou tricksu_old turkeycamp underconstruction registration].each do |page|
+  %w[gymnastics tumblebunnies tag dance preschool_dance swim events locations site_comments hosting competitive teamgym birthdays princess_party camps dancecamps campus employment forms missing nutcracker polkadots recitals recital_ad_order_thank_you show_schedules survey_thank_you summer thankyou tricksu_old turkeycamp underconstruction registration].each do |page|
     get page, controller: "static", action: page
   end
 
-  get 'dance_company' => 'dance_company#index'
-  %w[index about auditions empty_page events gallery].each do |page|
-    get 'dance_company/'+page, controller: 'dance_company', action: page
-  end
+  # get 'dance_company' => 'dance_company#index'
+  # %w[index about auditions empty_page events gallery].each do |page|
+  #   get 'dance_company/'+page, controller: 'dance_company', action: page
+  # end
   
 
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
-  get 'whereintheworld', to: 'worlds#new'
+  # get 'whereintheworld', to: 'worlds#new'
   get 'tricksu', to: 'tricks_u_videos#index', as: 'tricksu'
 
   get 'tricksu/:category' => "tricks_u_videos#index"
 
-  get 'survey/:id/start' => 'survey_results#new', as: 'start_survey'
-  get 'survey/:id/results' => 'survey_results#results_page', as: 'results_page'
+  # get 'survey/:id/start' => 'survey_results#new', as: 'start_survey'
+  # get 'survey/:id/results' => 'survey_results#results_page', as: 'results_page'
   
   get 'levels/jr_request/:level_id' => "levels#get_jr_classes"
 end
