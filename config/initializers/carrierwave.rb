81c8bad05e84e9f4977dd02a9a0d51667aad483d
Jordan Taylor <football80@gmail.com>
2017-08-22 20:09:57 +0000

    Fixing bug with Fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,14 +5,14 @@ if Rails.env.development? or Rails.env.test?
   end
 else
   CarrierWave.configure do |config|
-    config.storage = :fog
-    # config.fog_provider = 'fog-google'
     config.fog_credentials = {
       :provider                         => 'Google',
       :google_storage_access_key_id     => ENV['GOOGLE_KEY_ID'],  
       :google_storage_secret_access_key => ENV['GOOGLE_ACCESS_KEY'],
       :persistent                       => false
     }
+    config.storage = :fog
+    # config.fog_provider = 'fog-google'
     config.fog_directory = 'tricks-images'
   end
 end
