f8b31576c870e44253ec3d6f545f93b973d041fc
Jordan Taylor <football80@gmail.com>
2017-10-07 20:19:52 +0000

    Trying to get emails working again...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Comments::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
-    port: 587,
+    port: 465,#587
     domain: "tricksgym.com",
     authentication: "plain",
     enable_starttls_auto: true,
