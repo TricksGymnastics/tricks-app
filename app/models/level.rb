98ee16658c0ca7ca498f6a90aa501cf3ba4b3783
Jordan Taylor <football80@gmail.com>
2018-05-10 21:12:39 +0000

    Allowing levels to have the same jackrabbit name

--- app/models/level.rb
+++ app/models/level.rb
@@ -4,7 +4,7 @@ class Level < ActiveRecord::Base
 	has_many :coaches, through: :coach_levels
   has_many :absents
 	belongs_to :classtype
-	validates_uniqueness_of :levelname, :jack_rabbit_name
+	#validates_uniqueness_of :levelname, :jack_rabbit_name
 	validates_presence_of :classtype_id, :levelname, :length, :price, :gender, :sort_order
   #attr_accessible :description, :short_description, :video_url, :classtype_id, :levelname, :length, :price, :age, :gender, :sort_order, :jack_rabbit_name, :color, :image, :show_registration
 	
