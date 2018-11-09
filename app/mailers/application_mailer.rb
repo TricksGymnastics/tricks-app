102034ba1c8446f93dd3c4aa5d2ec02aab20f507
Jordan Taylor <football80@gmail.com>
2018-11-09 00:56:08 +0000

    Updating ApplicationEmailer get_location_email function

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,14 +2,14 @@ class ApplicationMailer < ActionMailer::Base
   default from: "Tricks Gymnastics, Dance & Swim <no_reply@tricksgym.com>"
   
   def get_location_email(location)
-    to_location = "football80@gmail.com"
     if (location == "Granite Bay")
-      to_address = "tricksgb@gmail.com"
+      return "tricksgb@gmail.com"
     elsif (location == "Folsom")
-      to_address = "tricksfol@gmail.com"
+      return "tricksfol@gmail.com"
     elsif (location == "Sacramento")
-      to_address = "trickssac@gmail.com"
+      return "trickssac@gmail.com"
+    else
+      return "football80@gmail.com"
     end
-    to_location
   end
 end
