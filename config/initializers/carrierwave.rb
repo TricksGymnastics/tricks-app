5cf58bb14c98f496ba31eb3c4ada4ceb19e94ce3
Jordan <football80@gmail.com>
2021-03-10 01:50:20 -0800

    Fixed permission issue with carrierwave/fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,4 @@
 require 'carrierwave/storage/fog'
-# https://stackoverflow.com/questions/52528623/migrating-carrierwave-to-activestorage
-
 # This stuff stays so that old existing images still work
 CarrierWave.configure do |config|
   if Rails.env.production? or Rails.env.development? #comment out the or if you are planning up testing upload
@@ -10,13 +8,6 @@ CarrierWave.configure do |config|
       :google_storage_secret_access_key => Rails.application.credentials.google_storage[:secret_access_key],
       # :persistent                       => false 
     }
-    
-#     config.fog_provider = 'fog/google'
-#     config.fog_credentials = {
-#       provider: 'Google',
-#       google_project: Rails.application.credentials.google_storage[:project_name],
-#       google_json_key_string: Rails.application.credentials.google_storage[:credential_content]
-#     }
     config.storage = :fog
 
     # config.fog_directory = Rails.application.credentials.google_storage[:default_bucket_name]
@@ -26,20 +17,3 @@ CarrierWave.configure do |config|
     config.enable_processing = true
   end
 end
-
-# CarrierWave.configure do |config|
-#   config.storage                             = :gcloud
-#   config.gcloud_bucket                       = Rails.application.credentials.google_storage[:default_bucket_name]
-#   config.gcloud_bucket_is_public             = true
-#   config.gcloud_authenticated_url_expiration = 600
-#   # config.gcloud_content_disposition          = 'attachment' // or you can skip this
-  
-#   config.gcloud_attributes = {
-#     expires: 600
-#   }
-  
-#   config.gcloud_credentials = {
-#     gcloud_project: Rails.application.credentials.google_storage[:project_name],
-#     gcloud_keyfile: Rails.application.credentials.google_storage[:credential_content]
-#   }
-# end
