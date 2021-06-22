de4723c97381a1f88abc5ac2322f19c1e2a2aa17
Jordan Taylor <football80@gmail.com>
2021-06-21 22:39:06 -0700

    Trying to improve error messages

--- app/mailers/error_mailer.rb
+++ app/mailers/error_mailer.rb
@@ -1,7 +1,8 @@
 class ErrorMailer < ApplicationMailer
-  def error_details(url, error, trace)
+  def error_details(url, error, exception_wrapper, trace)
     @url = url
     @error = error
+    @exception_wrapper = exception_wrapper
     @trace = trace
     @message = error.nil? ? "No Error" : error.message
     mail to: "trickswebmaster@gmail.com", subject: "Website Error: " + @message
